35f1d3b041cfd2af6cb7d8b5922e2ad5c59a3a80
Subah <subahcse11@gmail.com>
2021-11-16 22:54:42 +0600

    3.55

---
+++ Noknok/app/src/main/java/com/example/noknok/Request.java
@@ -0,0 +1,16 @@
+package com.example.noknok;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request);
+
+
+    }
+}
